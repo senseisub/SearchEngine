019a7fc36a6a46f297612a03bf2f98972bc49303
Brandon_Herman <brandonherman0202@gmail.com>
2020-11-16 20:10:58 -0600

    Added empty hashtable template

---
+++ HashTable.h
@@ -0,0 +1,12 @@
+//
+// Created by Brandon Herman on 11/16/20.
+//
+
+#ifndef SEARCH_ENGINE_PLUSPLUS_HASHTABLE_H
+#define SEARCH_ENGINE_PLUSPLUS_HASHTABLE_H
+
+// creating hash table template. will fill out tuesday with seun
+// just adding to repo
+
+
+#endif //SEARCH_ENGINE_PLUSPLUS_HASHTABLE_H
